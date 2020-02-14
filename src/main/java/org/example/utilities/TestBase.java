d4ef79c87cd94d386ee150e53f1ad137eced8f79
mike <mikael.conrath13@gmail.com>
2020-02-13 19:14:04 -0500

    Added Docker support

--- src/main/java/org/example/utilities/TestBase.java
+++ src/main/java/org/example/utilities/TestBase.java
@@ -4,32 +4,47 @@ import org.example.pages.GooglePage;
 import org.example.pages.NHLPage;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase extends PageBase {
   private static final ConfigReader configReader = new ConfigReader();
   public WebDriver driver = null;
+  public static String dockerUpCommand = "cmd /c start dockerUp.bat";
+  public static String dockerDownCommand = "cmd /c start dockerDown.bat";
+  public static String scaleCommand = "cmd /c start scaleChrome.bat";
+  public static String dockerReady = "The node is registered to the hub and ready to use";
+  public static String foundText = "Found text: ";
+  public static String outputFile = "output.txt";
+  public static String fileDeleted = "File deleted successfully";
+  public static Runtime runtime = Runtime.getRuntime();
   public static GooglePage google = null;
   public static NHLPage nhl = null;
 
   @BeforeMethod
-  public void setUp(ITestResult testResult) {
+  public void setUp(ITestResult testResult) throws MalformedURLException {
     test = extent.createTest(testResult.getMethod().getMethodName());
     System.setProperty("webdriver.chrome.driver", ConfigReader.getDriverPathChrome());
 
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("headless");
-
-    driver = new ChromeDriver(options);
+    URL url = new URL("http://localhost:4444/wd/hub");
+    DesiredCapabilities cap = DesiredCapabilities.chrome();
+    driver = new RemoteWebDriver(url, cap);
 
     driver.manage().window().setSize(new Dimension(1920, 1080));
     driver.manage().window().maximize();
@@ -40,8 +55,17 @@ public class TestBase extends PageBase {
   }
 
   @BeforeSuite
-  public void initBase() {
+  public void initBase() throws IOException, InterruptedException {
+    deleteOutput();
+    dockerCommand(dockerUpCommand, dockerReady, foundText);
+    scaleChrome(scaleCommand);
+
     initializeExtent();
+  }
+
+  @AfterTest
+  public void dockerDown() throws IOException, InterruptedException {
+    dockerCommand(dockerDownCommand, fileDeleted, foundText);
   }
 
   @AfterMethod
@@ -58,5 +82,49 @@ public class TestBase extends PageBase {
 
     driver.manage().deleteAllCookies();
     driver.quit();
+  }
+
+  public void dockerCommand(String dockerStatus, String dockerReady, String foundText)
+          throws IOException, InterruptedException {
+    boolean flag = false;
+
+    runtime.exec(dockerStatus);
+
+    Calendar cal = Calendar.getInstance();
+    cal.add(Calendar.SECOND, 45);
+    long stop = cal.getTimeInMillis();
+    Thread.sleep(3000);
+
+    while (System.currentTimeMillis() < stop) {
+      if (flag) {
+        break;
+      }
+
+      BufferedReader reader = new BufferedReader(new FileReader(outputFile));
+      String currentLine = reader.readLine();
+      while (currentLine != null && !flag) {
+        if (currentLine.contains(dockerReady)) {
+          System.out.println(foundText + dockerReady);
+          flag = true;
+          break;
+        }
+        currentLine = reader.readLine();
+      }
+      reader.close();
+    }
+
+    Assert.assertTrue(flag);
+  }
+
+  public void scaleChrome(String scaleCommand) throws IOException, InterruptedException {
+    runtime.exec(scaleCommand);
+    Thread.sleep(5000);
+  }
+
+  public void deleteOutput() {
+    File file = new File(outputFile);
+    if (file.delete()) {
+      System.out.println(fileDeleted);
+    }
   }
 }
